8531b293e103043f76e5ae14d407510995833c62
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-02-29 02:53:04 +0700

    5

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -15,17 +15,58 @@ public class Main {
             else if (text.equals("/help")) {
                 System.out.println("The program calculates the sum of numbers");
             } else {
-                text = text.replaceAll("\\+\\+|--", "");
-                text = text.replaceAll("-\\s+", "-");
-                text = text.replaceAll("\\+\\s+", "+");
-                String[] numbers = text.split("\\s+");
-                int result = 0;
-                for (String element : numbers) {
-                    result += Integer.parseInt(element);
+                try {
+                    if (text.matches("^/.*")) {
+                        throw new UnknownCommandException();
+                    }
+                    calculate(text);
+                } catch (UnknownCommandException | InvalidExpressionException e) {
+                    System.out.println(e.getMessage());
                 }
-                System.out.println(result);
             }
         }
         System.out.println("Bye!");
+    }
+
+    private static void calculate(String text) throws InvalidExpressionException {
+        text = text.replaceAll("\\s+", "");
+        text = text.replaceAll("--", "+");
+        text = text.replaceAll("\\+{2,}", "+");
+        text = text.replaceAll("\\+-|-\\+", "-");
+        if (!text.matches("^[-+].*")) {
+            text = "+" + text;
+        }
+
+        text = text.replaceAll("-", " -");
+        text = text.replaceAll("\\+", " +");
+        String[] numbers = text.trim().split("\\s+");
+
+        int result = 0;
+        for (String element : numbers) {
+
+            if (!element.matches("^[+-].*")) {
+                throw new InvalidExpressionException();
+            }
+
+            try {
+                result += Integer.parseInt(element);
+            } catch (NumberFormatException e) {
+                throw new InvalidExpressionException();
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    static class UnknownCommandException extends RuntimeException {
+        public UnknownCommandException() {
+            super("Unknown command");
+        }
+    }
+
+    static class InvalidExpressionException extends RuntimeException {
+        public InvalidExpressionException() {
+            super("Invalid expression");
+        }
     }
 }
