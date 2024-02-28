f57fc053de2eb8a9e3b5d11a8b6ec59901293b52
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-02-29 02:32:38 +0700

    2

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -1,11 +1,21 @@
 package calculator;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() + sc.nextInt());
+        String text;
+        while (!Objects.equals(text = sc.nextLine(), "/exit")) {
+            if (text.isBlank()) continue;
+            String[] numbers = text.split("\\s+");
+            switch (numbers.length) {
+                case 1 -> System.out.println(Integer.parseInt(numbers[0]));
+                case 2 -> System.out.println(Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]));
+            }
+        }
+        System.out.println("Bye!");
     }
 }
