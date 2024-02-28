b8073911aa197b0ac85a9ce7ef4f4aeb4f7fbf7d
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-02-29 02:41:04 +0700

    3

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -9,11 +9,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String text;
         while (!Objects.equals(text = sc.nextLine(), "/exit")) {
-            if (text.isBlank()) continue;
-            String[] numbers = text.split("\\s+");
-            switch (numbers.length) {
-                case 1 -> System.out.println(Integer.parseInt(numbers[0]));
-                case 2 -> System.out.println(Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]));
+            if (text.isBlank()) {
+                continue;
+            }
+            else if (text.equals("/help")) {
+                System.out.println("The program calculates the sum of numbers");
+            } else {
+                String[] numbers = text.split("\\s+");
+                int result = 0;
+                for (String element : numbers) {
+                    result += Integer.parseInt(element);
+                }
+                System.out.println(result);
             }
         }
         System.out.println("Bye!");
