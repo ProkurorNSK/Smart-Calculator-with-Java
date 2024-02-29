a063e71186cb41911fb7fbff4d58ee907d851ea9
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-03-01 01:49:36 +0700

    7

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -2,6 +2,7 @@ package calculator;
 
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
 
@@ -22,7 +23,9 @@ public class Main {
                     try {
                         input = input.replaceAll("\\s+", "");
                         if (parseInput(input)) {
+//                            printStack(infix);
                             convertToPostfix();
+//                            printStack(postfix);
                             calculate();
                         }
                     } catch (CalculatorException e) {
@@ -34,22 +37,73 @@ public class Main {
         System.out.println("Bye!");
     }
 
-    private static void convertToPostfix() {
+    private static void printStack(List<Element> elements) {
+        elements.forEach(element -> System.out.print(element + " "));
+        System.out.println();
+    }
+
+    private static void calculate() {
+        Deque<Element> stack = new ArrayDeque<>();
+
+        for (Element element : postfix) {
+            switch (element.type) {
+                case NUMBER, VARIABLE -> stack.push(element);
+                case BINARY_OPERATOR -> {
+                    int x = ((Number) stack.pop()).number;
+                    int y = ((Number) stack.pop()).number;
+                    stack.push(new Number(Type.NUMBER, ((BinaryOperator) element).biOperator.apply(y, x)));
+                }
+                case UNARY_OPERATOR -> {
+                    int y = ((Number) stack.pop()).number;
+                    stack.push(new Number(Type.NUMBER, ((UnaryOperator) element).unOperator.apply(y)));
+                }
+            }
+        }
+
+        System.out.println(((Number) stack.pop()).number);
+    }
+
+    private static void convertToPostfix() throws CalculatorException {
         postfix.clear();
         Deque<Element> stack = new ArrayDeque<>();
         for (Element element : infix) {
-            switch (element.type()) {
-                case VARIABLE, NUMBER -> postfix.add(element);
-                case BINARY_OPERATOR -> {
-                    if (!stack.isEmpty()) {
+            switch (element.type) {
+                case VARIABLE, NUMBER -> {
+                    postfix.add(element);
+                    while (!stack.isEmpty() && stack.peek().type == Type.UNARY_OPERATOR) {
                         postfix.add(stack.pop());
                     }
-                    stack.push(element);
+                }
+                case BINARY_OPERATOR -> {
+                    if (stack.isEmpty() || ((Operator)stack.peek()).priority < ((Operator)element).priority) {
+                        stack.push(element);
+                    } else {
+                        while (!stack.isEmpty() && ((Operator)stack.peek()).priority >= ((Operator)element).priority) {
+                            postfix.add(stack.pop());
+                        }
+                        stack.push(element);
+                    }
+                }
+                case UNARY_OPERATOR, LEFT_PARENTHESIS -> stack.push(element);
+                case RIGHT_PARENTHESIS -> {
+                    while (!(stack.isEmpty() || stack.peek().type == Type.LEFT_PARENTHESIS)) {
+                        postfix.add(stack.pop());
+                    }
+                    if (stack.isEmpty() || stack.peek().type != Type.LEFT_PARENTHESIS) {
+                        throw new CalculatorException("Invalid expression");
+                    } else {
+                        stack.pop();
+                    }
                 }
             }
         }
         while (!stack.isEmpty()) {
-            postfix.add(stack.pop());
+            Element element = stack.pop();
+            if (element.type == Type.LEFT_PARENTHESIS || element.type == Type.RIGHT_PARENTHESIS) {
+                throw new CalculatorException("Invalid expression");
+            } else {
+                postfix.add(element);
+            }
         }
     }
 
@@ -81,18 +135,44 @@ public class Main {
         Type currentType = Type.BINARY_OPERATOR;
         for (char ch : input.toCharArray()) {
             if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^') {
-                addToken(currentType, token);
-                token = "";
-                currentType = Type.BINARY_OPERATOR;
-                BiFunction<Integer, Integer, Integer> function = switch (ch) {
-                    case '+' -> Integer::sum;
-                    case '-' -> (x, y) -> x - y;
-                    case '*' -> (x, y) -> x * y;
-                    case '/' -> (x, y) -> x / y;
-                    case '^' -> (x, y) -> (int) Math.pow(x, y);
-                    default -> null;
-                };
-                infix.add(new Element(currentType, 0, String.valueOf(ch), function));
+                if (currentType == Type.VARIABLE || currentType == Type.NUMBER || currentType == Type.RIGHT_PARENTHESIS) {
+                    addToken(currentType, token);
+                    token = "";
+                    currentType = Type.BINARY_OPERATOR;
+                    int priority = 0;
+                    BiFunction<Integer, Integer, Integer> function = null;
+                    switch (ch) {
+                        case '+' -> {
+                            function = Integer::sum;
+                            priority = 1;
+                        }
+                        case '-' -> {
+                            function = (x, y) -> x - y;
+                            priority = 1;
+                        }
+                        case '*' -> {
+                            function = (x, y) -> x * y;
+                            priority = 2;
+                        }
+                        case '/' -> {
+                            function = (x, y) -> x / y;
+                            priority = 2;
+                        }
+                        case '^' -> {
+                            function = (x, y) -> (int) Math.pow(x, y);
+                            priority = 3;
+                        }
+                    }
+                    infix.add(new BinaryOperator(currentType, priority, function, ch));
+                } else {
+                    currentType = Type.UNARY_OPERATOR;
+                    Function<Integer, Integer> function = switch (ch) {
+                        case '+' -> x -> x;
+                        case '-' -> x -> -1 * x;
+                        default -> throw new CalculatorException("Invalid expression");
+                    };
+                    infix.add(new UnaryOperator(currentType, 4, function, ch));
+                }
             } else if (Character.isDigit(ch)) {
                 if (currentType == Type.VARIABLE) {
                     addToken(currentType, token);
@@ -107,6 +187,16 @@ public class Main {
                 }
                 currentType = Type.VARIABLE;
                 token = token.concat(String.valueOf(ch));
+            } else if (ch == '(') {
+                currentType = Type.LEFT_PARENTHESIS;
+                infix.add(new Operator(currentType, 0, ch));
+            } else if (ch == ')') {
+                addToken(currentType, token);
+                token = "";
+                currentType = Type.RIGHT_PARENTHESIS;
+                infix.add(new Operator(currentType, 0, ch));
+            } else {
+                throw new CalculatorException("Invalid expression");
             }
         }
 
@@ -118,43 +208,97 @@ public class Main {
     }
 
     private static void addToken(Type type, String token) throws CalculatorException {
+        if (token.isEmpty()) {
+            return;
+        }
+
         switch (type) {
             case NUMBER -> {
                 try {
-                    infix.add(new Element(type, Integer.parseInt(token), token, null));
+                    infix.add(new Number(type, Integer.parseInt(token)));
                 } catch (NumberFormatException e) {
                     throw new CalculatorException("Invalid expression");
                 }
             }
             case VARIABLE -> {
                 if (variables.containsKey(token)) {
-                    infix.add(new Element(type, variables.get(token), token, null));
+                    infix.add(new Variable(type, variables.get(token), token));
                 } else {
                     throw new CalculatorException("Invalid expression");
                 }
             }
         }
     }
+}
 
-    private static void calculate() {
-        Deque<Element> stack = new ArrayDeque<>();
+class Element {
+    Type type;
 
-        for (Element element : postfix) {
-            switch (element.type()) {
-                case NUMBER, VARIABLE -> stack.push(element);
-                case BINARY_OPERATOR -> {
-                    int x = stack.pop().number();
-                    int y = stack.pop().number();
-                    stack.push(new Element(Type.NUMBER, element.biOperator().apply(y, x), "", null));
-                }
-            }
-        }
-
-        System.out.println(stack.pop().number());
+    Element(Type type) {
+        this.type = type;
     }
 }
 
-record Element(Type type, int number, String variable, BiFunction<Integer, Integer, Integer> biOperator) {
+class Number extends Element {
+    int number;
+
+    Number(Type type, int number) {
+        super(type);
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+}
+
+class Variable extends Number {
+    String variable;
+
+    Variable(Type type, int number, String variable) {
+        super(type, number);
+        this.variable = variable;
+    }
+
+    @Override
+    public String toString() {
+        return variable;
+    }
+}
+
+class Operator extends Element {
+    int priority;
+    String symbol;
+
+    Operator(Type type, int priority, char symbol) {
+        super(type);
+        this.priority = priority;
+        this.symbol = String.valueOf(symbol);
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+}
+
+class BinaryOperator extends Operator {
+    BiFunction<Integer, Integer, Integer> biOperator;
+
+    BinaryOperator(Type type, int priority, BiFunction<Integer, Integer, Integer> biOperator, char symbol) {
+        super(type, priority, symbol);
+        this.biOperator = biOperator;
+    }
+}
+
+class UnaryOperator extends Operator {
+    Function<Integer, Integer> unOperator;
+
+    UnaryOperator(Type type, int priority, Function<Integer, Integer> unOperator, char symbol) {
+        super(type, priority, symbol);
+        this.unOperator = unOperator;
+    }
 }
 
 enum Type {
